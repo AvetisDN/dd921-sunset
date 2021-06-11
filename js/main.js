2909fac4488a8fa7a8c3e8927ff656d062c36356
AvetisDN <avetisdn@gmail.com>
2021-06-11 17:13:54 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 const menu = document.querySelector('.main-menu')
+const menuBtn = document.querySelector('.btn-burger')
+const menuList = document.querySelector('.menu-list')
+const menuShadow = document.querySelector('.menu-shadow')
+
+let menuOpened = false
+
+menuBtn.onclick = (e) => {
+    menuList.classList.add('show')
+    menuShadow.classList.add('show')
+    document.body.classList.add('stop-scrolling')
+}
+menuShadow.onclick = (e) => {
+    menuList.classList.remove('show')
+    menuShadow.classList.remove('show')
+    document.body.classList.remove('stop-scrolling')
+}
+
+document.addEventListener('swiped-left', function(e) {
+    menuList.classList.remove('show')
+    menuShadow.classList.remove('show')
+    document.body.classList.remove('stop-scrolling')
+});
+document.addEventListener('swiped-right', function(e) {
+    menuList.classList.add('show')
+    menuShadow.classList.add('show')
+    document.body.classList.add('stop-scrolling')
+});
+
 
 const checkScroll = (e) => {
     if(window.pageYOffset > window.innerHeight - 60) {
@@ -10,25 +38,3 @@ const checkScroll = (e) => {
 
 window.onscroll = checkScroll
 window.onload = checkScroll
-
-const menuBtn = document.querySelector('.btn-burger')
-const menuList = document.querySelector('.menu-list')
-const menuShadow = document.querySelector('.menu-shadow')
-
-menuBtn.onclick = (e) => {
-    menuList.classList.toggle('show')
-    menuShadow.classList.toggle('show')
-}
-menuShadow.onclick = (e) => {
-    menuList.classList.remove('show')
-    menuShadow.classList.remove('show')
-}
-
-document.addEventListener('swiped-left', function(e) {
-    menuList.classList.remove('show')
-    menuShadow.classList.remove('show')
-});
-document.addEventListener('swiped-right', function(e) {
-    menuList.classList.add('show')
-    menuShadow.classList.add('show')
-});
